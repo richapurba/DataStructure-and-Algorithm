a7e7be0ee3551ac187a7799aee3831a5955affbc
Richa Purba <richathorne@gmail.com>
2021-03-05 16:25:11 +0000

    Add brief explanation

---
+++ Basics/timeComplexity.js
@@ -0,0 +1,32 @@
+// Nowadays, time complexity of our code can be considered as Big-O.
+// Big-O is important for analyzing and comparing the efficiencies of algorithms.
+// The analysis of Big-O starts by looking at the code and applying the rules to simplify the Big-O notation.
+// The following are the most often used rules:
+// 1. Eliminating coefficients/ constants (coefficient rule)
+// 2. Adding up Big-O (sum rule)
+// 3. Multiplying Big-O (product rule)
+// 4. Determining the polynomial of the Big-O notation by looking at loops (polynomial rule)
+
+// BASE RULES
+// First sample: (CONSTANT ONLY)
+x = x + 1;	// The output of the code depends on x value; then we might considered the time complexity as a constant value. Therefore it must be (O) = 1
+
+// Second sample: (COEFFICIENT RULE)
+function action() {
+	return x;
+}
+
+// Third sample: (SUM RULE)
+function acting() {
+	return x;
+}
+
+// Fourth sample: (PRODUCT RULE)
+function actrees() {
+	return x;
+}
+
+// Fifth sample: ()
+function popcorn() {
+	return x;
+}
